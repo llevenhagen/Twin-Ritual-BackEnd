d821c6c19b5e82966ddcfc9faba657da1ce3e116
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-11-26 17:20:25 -0800

    added to index.js db.use_env_variable

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,7 @@ const config = require('../config/config')
 const db = {}
 
 const sequelize = new Sequelize(
+  config.use_env_variable,
   config.db.database,
   config.db.user,
   config.db.password,
