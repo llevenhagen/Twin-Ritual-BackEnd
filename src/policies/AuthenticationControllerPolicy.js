42b6baff543e2d2880f9ff127408a16c001fd585
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-07 00:26:15 -0800

    added admin to joi validation

--- src/policies/AuthenticationControllerPolicy.js
+++ src/policies/AuthenticationControllerPolicy.js
@@ -6,7 +6,8 @@ module.exports = {
       email: Joi.string().email(),
       password: Joi.string().regex(
         new RegExp('^[a-zA-Z0-9]{8,32}$')
-      )
+      ),
+      admin: Joi.boolean()
     }
 
     const { error } = Joi.validate(req.body, schema)
