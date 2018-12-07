45fbcfba6b2b832b53ae1bdb108e1d966ec68252
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-12-06 23:59:24 -0800

    confusing myself

--- src/models/cart.js
+++ src/models/cart.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const cart = sequelize.define('cart', {
     merchId: DataTypes.INTEGER,
-    userID: DataTypes.INTEGER
+    userId: DataTypes.INTEGER
   })
   return cart
 }
